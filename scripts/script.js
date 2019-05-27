34f11fc711a2901d7a30bcd6f606a0991bf25b7a
lbCyber <paul@lintbox.com>
2019-05-26 23:26:12 -0400

    Minor fix, hide subheader text on submit, added placeholder for dropdown

--- scripts/script.js
+++ scripts/script.js
@@ -75,6 +75,7 @@ myApp.getGenres = function () {
     for (item in response) {
       $('select').append(`<option value="${response[item]}">${item}</option>`);
     }
+    $('select').append(`<option value="null" style="display:none" selected disabled>Choose a genre</option>`);
   })
 }
 
